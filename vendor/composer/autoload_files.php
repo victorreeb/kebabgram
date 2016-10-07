5565aae51b936ab4f2d62a5c6a1eff9bb041a67b
Pi <pierrebecker57540@gmail.com>
2016-10-07 15:16:13 +0200

    ajout slim

--- vendor/composer/autoload_files.php
+++ vendor/composer/autoload_files.php
@@ -9,4 +9,5 @@ return array(
     $vendorDir . '/symfony/polyfill-mbstring/bootstrap.php',
     $vendorDir . '/paragonie/random_compat/lib/random.php',
     $vendorDir . '/illuminate/support/helpers.php',
+    $vendorDir . '/nikic/fast-route/src/functions.php',
 );
