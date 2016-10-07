d4c298483ef84e46fdfaec2027ba1384a482ced6
v_reeb <victor.reeb@live.fr>
2016-10-07 23:39:08 +0200

    tri du dossier && utilisation de twig

--- src/routes.php
+++ src/routes.php
@@ -1,9 +1,13 @@
 <?php
 // Routes
 
+// $app->get('/', function ($request, $response, $args) {
+//     // Sample log message
+//     $this->logger->info("Homepage route");
+//     // Render views
+//     return $this->renderer->render($response, 'homepage.phtml', $args);
+// });
+
 $app->get('/', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Homepage route");
-    // Render views
-    return $this->renderer->render($response, 'homepage.phtml', $args);
+    return $this->view->render($response, 'homepage.html');
 });
