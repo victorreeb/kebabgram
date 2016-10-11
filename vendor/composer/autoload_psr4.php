892a36bcadc4dadb7209fb9ac264bf927f985484
v_reeb <victor.reeb@live.fr>
2016-10-11 23:40:01 +0200

    add token Slim && update on views

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -14,6 +14,7 @@ return array(
     'Symfony\\Component\\Yaml\\' => array($vendorDir . '/symfony/yaml'),
     'Symfony\\Component\\Translation\\' => array($vendorDir . '/symfony/translation'),
     'Slim\\Views\\' => array($vendorDir . '/slim/php-view/src', $vendorDir . '/slim/twig-view/src'),
+    'Slim\\Csrf\\' => array($vendorDir . '/slim/csrf/src'),
     'Slim\\' => array($vendorDir . '/slim/slim/Slim'),
     'Respect\\Validation\\' => array($vendorDir . '/respect/validation/library'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
