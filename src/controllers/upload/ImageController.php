aa417218cdc8193d0e4a6d915e6575182e8b73f5
v_reeb <victor.reeb@live.fr>
2016-11-07 15:22:33 +0100

    ajout image upload 1st step

--- src/controllers/upload/ImageController.php
+++ src/controllers/upload/ImageController.php
@@ -3,6 +3,7 @@
 namespace Controllers\Upload;
 
 use \Controllers\Controller;
+use Models\User;
 
 class ImageController extends Controller{
 
@@ -13,9 +14,37 @@ class ImageController extends Controller{
   }
 
   public function postAddImage($request, $response){
-    echo "Post Image \n";
-    $files = $request->getUploadedFiles();
-    var_dump($files['file_image']);
+
+    $user = User::find($_SESSION['user']);
+
+    // overwrite = true
+    $storage = new \Upload\Storage\FileSystem($_SERVER['DOCUMENT_ROOT'] . 'kebabgram/public/uploads', true);
+    $file = new \Upload\File('file_image', $storage);
+
+    $file->setName(strtolower($user->name) . '_' . 'test');
+
+    $file->addValidations(array(
+        new \Upload\Validation\Mimetype(array('image/png', 'image/jpeg')),
+        new \Upload\Validation\Size('5M')
+    ));
+
+    $data = array(
+        'name'       => $file->getNameWithExtension(),
+        'extension'  => $file->getExtension(),
+        'mime'       => $file->getMimetype(),
+        'size'       => $file->getSize(),
+        'md5'        => $file->getMd5(),
+        'dimensions' => $file->getDimensions()
+    );
+
+    try {
+        $file->upload();
+        $this->flash->addMessage('success', 'Your image is uploaded');
+        return $response->withRedirect($this->router->pathFor('home'));
+    } catch (\Exception $e) {
+        $this->flash->addMessage('error', 'Some errors have been detected.');
+        return $response->withRedirect($this->router->pathFor('auth.images.add'));
+    }
   }
 
 }
