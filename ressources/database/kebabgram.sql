7b5cf1f4dbc575bc61cf79c3f4c5be4512733302
v_reeb <victor.reeb@live.fr>
2016-11-08 10:07:40 +0100

    update db file

--- ressources/database/kebabgram.sql
+++ ressources/database/kebabgram.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost
--- Généré le :  Jeu 20 Octobre 2016 à 08:00
--- Version du serveur :  5.7.15-0ubuntu0.16.04.1
--- Version de PHP :  7.0.8-0ubuntu0.16.04.3
+-- Client :  127.0.0.1
+-- Généré le :  Mar 08 Novembre 2016 à 09:06
+-- Version du serveur :  5.7.9
+-- Version de PHP :  5.6.16
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,13 +26,18 @@ SET time_zone = "+00:00";
 -- Structure de la table `photos`
 --
 
-CREATE TABLE `photos` (
-  `id` int(11) NOT NULL,
-  `link` int(11) NOT NULL,
-  `name` int(11) NOT NULL,
-  `place` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `photos`;
+CREATE TABLE IF NOT EXISTS `photos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tag` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `place` varchar(255) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `extension` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL,
+  `updated_at` timestamp NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -40,53 +45,17 @@ CREATE TABLE `photos` (
 -- Structure de la table `users`
 --
 
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
---
--- Contenu de la table `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `password`, `created_at`, `updated_at`) VALUES
-(5, 'Victor', 'victor@test.fr', '$2y$10$n1k.TZmDLgiSjnrtOTCrauQpznYC2aALP9bZDomaNYbmQ84lM.FWu', '2016-10-17 15:33:39', '2016-10-17 15:33:39'),
-(6, 'Giant2', 'giant2@test.fr', '$2y$10$S.bdmGJo3NrmJ3JzRz8FKuxyavA0ZMlywlRdXhlJQH7jErmxyMqQe', '2016-10-17 15:35:06', '2016-10-17 15:35:06');
-
---
--- Index pour les tables exportées
---
-
---
--- Index pour la table `photos`
---
-ALTER TABLE `photos`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables exportées
---
-
---
--- AUTO_INCREMENT pour la table `photos`
---
-ALTER TABLE `photos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
