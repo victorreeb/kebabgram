13d7591f2c260eb69828551ec412d39283816be6
reeb5u <victor.reeb@live.fr>
2016-11-12 23:32:39 +0100

    ajout auth/guestMiddleware to protect url

--- src/routes.php
+++ src/routes.php
@@ -1,22 +1,38 @@
 <?php
 
+use Middleware\AuthMiddleware;
+use Middleware\GuestMiddleware;
+
+//GuestMiddleware
+$app->group('', function(){
+
+  //Auth
+  $this->get('/auth/signup', 'AuthController:getSignUp')->setname('auth.signup');
+  $this->post('/auth/signup', 'AuthController:postSignUp');
+
+  $this->get('/auth/signin', 'AuthController:getSignIn')->setname('auth.signin');
+  $this->post('/auth/signin', 'AuthController:postSignIn');
+
+})->add(new GuestMiddleware($container));
+
+//AuthMiddleware
+$app->group('', function(){
+
+  //Auth
+  $this->get('/auth/signout', 'AuthController:getSignOut')->setname('auth.signout');
+
+  $this->get('/auth/password/change', 'PasswordController:getChangePassword')->setname('auth.password.change');
+  $this->post('/auth/password/change', 'PasswordController:postChangePassword');
+
+  $this->get('/auth/images/add', 'ImageController:getAddImage')->setname('auth.images.add');
+  $this->post('/auth/images/add', 'ImageController:postAddImage');
+  $this->post('/auth/images/{id}/delete', 'ImageController:delete_images_user')->setname('auth.images.delete');
+  $this->post('/{name}/images/{id}/noter', 'ImageController:noter')->setname('image.noter');
+
+})->add(new AuthMiddleware($container));
+
 // Homepage
 $app->get('/', 'HomepageController:index')->setname('home');
-
-//Auth
-$app->get('/auth/signup', 'AuthController:getSignUp')->setname('auth.signup');
-$app->post('/auth/signup', 'AuthController:postSignUp');
-
-$app->get('/auth/signin', 'AuthController:getSignIn')->setname('auth.signin');
-$app->post('/auth/signin', 'AuthController:postSignIn');
-
-$app->get('/auth/signout', 'AuthController:getSignOut')->setname('auth.signout');
-
-//Import
-$app->get('/auth/images/add', 'ImageController:getAddImage')->setname('auth.images.add');
-$app->post('/auth/images/add', 'ImageController:postAddImage');
-$app->post('/auth/images/{id}/delete', 'ImageController:delete_images_user')->setname('auth.images.delete');
-
 
 //Profil
 $app->get('/profil', 'ProfilController:index')->setname('auth.profil');
@@ -27,10 +43,3 @@ $app->get('/images/bestof', 'ImageController:index_best')->setname('images.besto
 $app->get('/{name}/images', 'ImageController:index_images_user')->setname('images.user.index');
 $app->get('/{name}/images/{id}', 'ImageController:show_images_user')->setname('images.user.show');
 $app->post('/{name}/images/{id}', 'ImageController:edit_images_user')->setname('images.user.edit');
-
-//notes
-$app->post('/{name}/images/{id}/noter', 'ImageController:noter')->setname('image.noter');
-
-//password
-$app->get('/auth/password/change', 'PasswordController:getChangePassword')->setname('auth.password.change');
-$app->post('/auth/password/change', 'PasswordController:postChangePassword');
